13c8d16276de616f603aa8df86e1897af4619af3
Tundroid <felixkimbu@outlook.com>
2024-10-16 21:26:42 +0100

    Cleanup... deleted: ./models/client_account copy.py

---
+++ models/user_account.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+""" ClientAccount class """
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, SmallInteger, Enum, Boolean, Integer, TIMESTAMP
+from sqlalchemy.sql import func
+
+
+class ClientAccount(BaseModel, Base):
+    """ Representation of Client Account """
+    if models.storage_t == "db":
+        __tablename__ = 'client_account'
+
+        id = Column(SmallInteger, primary_key=True, autoincrement=True)
+        acc_name = Column(String(50), nullable=False, unique=True)
+        acc_type = Column(Enum('Consumer', 'Worker', 'Both'), nullable=False, default='Consumer')
+        is_active = Column(Boolean, nullable=False, default=True)
+        credit_limit = Column(Integer, nullable=False, default=25000)
+        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+
+    def __init__(self, *args, **kwargs):
+        """ ClientAccount initialization """
+        super().__init__(*args, **kwargs)
