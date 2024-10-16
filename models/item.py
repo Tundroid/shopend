890dd3efc8822a3d2876cbd81274b2d2ffdb73f5
Tundroid <felixkimbu@outlook.com>
2024-10-16 20:04:37 +0100

    Cleanup... deleted: ./models/supplier copy.py

---
+++ models/item.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+""" Supplier class """
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Text, SmallInteger, ForeignKey, TIMESTAMP
+from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
+
+
+class Supplier(BaseModel, Base):
+    """ Representation of Supplier """
+    if models.storage_t == "db":
+        __tablename__ = 'supplier'
+
+        id = Column(SmallInteger, primary_key=True, autoincrement=True)
+        sup_name = Column(String(50), nullable=False, unique=True)
+        sup_type = Column(SmallInteger, ForeignKey('sup_type.id'), nullable=False)
+        sup_desc = Column(Text)
+        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+
+        sup_type_rel = relationship('SupplierType', backref='suppliers')
+
+    def __init__(self, *args, **kwargs):
+        """ Supplier initialization """
+        super().__init__(*args, **kwargs)
