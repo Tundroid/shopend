f85932ba6040478e86cb205d684a7b16b665f80d
Tundroid <felixkimbu@outlook.com>
2024-10-16 16:29:41 +0100

    Some change...

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,7 @@ initialize the models package
 
 from os import getenv
 
-storage_t = getenv("CGCEB_TYPE_STORAGE")
+storage_t = "db"
 
 if storage_t == "db":
     from models.engine.db_storage import DBStorage
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,7 +71,6 @@ class FileStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
-        print("testing if na you di work")
         if cls not in classes.values():
             return None
 
