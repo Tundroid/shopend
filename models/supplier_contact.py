df02f51530409ea9faae2f7135e8f8a9dda70640
Tundroid <felixkimbu@outlook.com>
2024-10-16 19:51:15 +0100

    Cleanup... deleted: ./models/sector copy.py

---
+++ models/supplier_contact.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+""" Sector class """
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Text, SmallInteger, TIMESTAMP
+from sqlalchemy.sql import func
+
+
+class Sector(BaseModel, Base):
+    """ Representation of Sector """
+    if models.storage_t == "db":
+        __tablename__ = 'sector'
+
+        id = Column(SmallInteger, primary_key=True, autoincrement=True)
+        sec_name = Column(String(50), nullable=False, unique=True)
+        sec_desc = Column(Text)
+        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+
+    def __init__(self, *args, **kwargs):
+        """ Sector initialization """
+        super().__init__(*args, **kwargs)
