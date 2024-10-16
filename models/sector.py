bb744c5770271855c9cec9031b6eeb5f1b5a6daa
Tundroid <felixkimbu@outlook.com>
2024-10-16 19:31:06 +0100

    Cleanup... deleted: ./models/item_cat copy.py

---
+++ models/sector.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+""" ItemCategory class """
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Text, TIMESTAMP, SmallInteger
+from sqlalchemy.sql import func
+
+
+class ItemCategory(BaseModel, Base):
+    """ Representation of Item Category """
+    if models.storage_t == "db":
+        __tablename__ = 'item_cat'
+
+        id = Column(SmallInteger, primary_key=True, autoincrement=True)
+        cat_name = Column(String(50), nullable=False, unique=True)
+        cat_desc = Column(Text)
+        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+
+    def __init__(self, *args, **kwargs):
+        """ ItemCategory initialization """
+        super().__init__(*args, **kwargs)
