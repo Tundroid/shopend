0e9f73057dae39e1a372d20d582eceb79a7f20c9
Tundroid <felixkimbu@outlook.com>
2024-10-16 16:23:41 +0100

    Some change...

--- api/v1/views/depot_details.py
+++ api/v1/views/depot_details.py
@@ -10,6 +10,7 @@ from api.v1.views import app_views
 @app_views.route("/depot_detail", methods=['GET'], strict_slashes=False)
 @app_views.route("/depot_detail/<depot_id>", methods=['GET'], strict_slashes=False)
 def get_depot_detail(depot_id=None):
+    print("Find me here")
     if depot_id:
         depot_detail = storage.get(DepotDetail, depot_id)
         if depot_detail:
