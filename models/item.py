d9cb5d87f41181eaebb0a7d950e750c96dc084d4
Tundroid <felixkimbu@outlook.com>
2024-10-16 20:07:27 +0100

    Some change...

--- models/item.py
+++ models/item.py
@@ -13,13 +13,13 @@ class Item(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'item'
 
-        id = Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
+        id = Column(Integer, primary_key=True, autoincrement=True)
         item_name = Column(String(50), nullable=False, unique=True)
         item_cat = Column(SmallInteger, ForeignKey('item_cat.id'), nullable=False, default=1)
-        cost_price = Column(Integer(unsigned=True), nullable=False, default=0)
-        selling_price = Column(Integer(unsigned=True), nullable=False, default=0)
-        min_sell_price = Column(Integer(unsigned=True), nullable=False, default=0)
-        min_order_qty = Column(Integer(unsigned=True), nullable=False, default=5)
+        cost_price = Column(Integer, nullable=False, default=0)
+        selling_price = Column(Integer, nullable=False, default=0)
+        min_sell_price = Column(Integer, nullable=False, default=0)
+        min_order_qty = Column(Integer, nullable=False, default=5)
         family = Column(SmallInteger, ForeignKey('family.id'), nullable=False, default=1)
         sector = Column(SmallInteger, ForeignKey('sector.id'), nullable=False, default=1)
         is_active = Column(Boolean, nullable=False, default=True)
