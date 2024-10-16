7a570d6226e4f649bb275682f3a4b3936f796fa0
Tundroid <felixkimbu@outlook.com>
2024-10-16 20:10:01 +0100

    Model: depot_map

--- models/depot_map.py
+++ models/depot_map.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
-""" DepotDetail class """
+""" DepotMap class """
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Text, Enum, Boolean, TIMESTAMP, SmallInteger
-from sqlalchemy.sql import func
+from sqlalchemy import Column, SmallInteger, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
 
 
-class DepotDetail(BaseModel, Base):
-    """ Representation of DepotDetail """
+class DepotMap(BaseModel, Base):
+    """ Representation of Depot Map """
     if models.storage_t == "db":
-        __tablename__ = 'depot_detail'
+        __tablename__ = 'depot_map'
 
-        id = Column('id', SmallInteger, primary_key=True, autoincrement=True)
-        depot_name = Column(String(50), nullable=False, unique=True)
-        depot_desc = Column(Text, nullable=False)
-        depot_type = Column(Enum('Source', 'Destination', 'Both'), nullable=False)
-        is_metered = Column(Boolean, nullable=False, default=True)
-        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+        source = Column(SmallInteger, ForeignKey('depot_detail.id'), nullable=False)
+        destination = Column(SmallInteger, ForeignKey('depot_detail.id'), nullable=False)
+        operation = Column(SmallInteger, ForeignKey('operation.id'), nullable=False)
+
+        __table_args__ = (
+            PrimaryKeyConstraint('source', 'destination', 'operation'),
+        )
+
+        depot_source_rel = relationship('DepotDetail', foreign_keys=[source], backref='depot_map_sources')
+        depot_destination_rel = relationship('DepotDetail', foreign_keys=[destination], backref='depot_map_destinations')
+        operation_rel = relationship('Operation', backref='depot_maps')
 
     def __init__(self, *args, **kwargs):
-        """ DepotDetail initialization """
+        """ DepotMap initialization """
         super().__init__(*args, **kwargs)
