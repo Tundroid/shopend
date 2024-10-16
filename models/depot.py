48a1b349a04db23e952baa3da0b74d01e7cfb41a
Tundroid <felixkimbu@outlook.com>
2024-10-16 20:11:05 +0100

    Cleanup... deleted: ./models/depot_map copy.py

---
+++ models/depot.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+""" DepotMap class """
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, SmallInteger, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
+
+
+class DepotMap(BaseModel, Base):
+    """ Representation of Depot Map """
+    if models.storage_t == "db":
+        __tablename__ = 'depot_map'
+
+        source = Column(SmallInteger, ForeignKey('depot_detail.id'), nullable=False)
+        destination = Column(SmallInteger, ForeignKey('depot_detail.id'), nullable=False)
+        operation = Column(SmallInteger, ForeignKey('operation.id'), nullable=False)
+
+        __table_args__ = (
+            PrimaryKeyConstraint('source', 'destination', 'operation'),
+        )
+
+        depot_source_rel = relationship('DepotDetail', foreign_keys=[source], backref='depot_map_sources')
+        depot_destination_rel = relationship('DepotDetail', foreign_keys=[destination], backref='depot_map_destinations')
+        operation_rel = relationship('Operation', backref='depot_maps')
+
+    def __init__(self, *args, **kwargs):
+        """ DepotMap initialization """
+        super().__init__(*args, **kwargs)
