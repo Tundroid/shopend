03e085d7a7aa33ad8d596e127b712998d62b0276
Tundroid <felixkimbu@outlook.com>
2024-10-16 17:31:37 +0100

    Some change...

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -106,6 +106,7 @@ class DBStorage:
         all_cls = models.storage.all(cls)
         for value in all_cls.values():
             obj_id = getattr(value, inspect(cls).primary_key[0].name)
+            print(id, obj_id)
             if (str(obj_id) == str(id)):
                 return value
 
