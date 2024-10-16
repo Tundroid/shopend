e0339b75ad6700ecd11afac699ac3a2887cdfe90
Tundroid <felixkimbu@outlook.com>
2024-10-16 21:22:50 +0100

    Cleanup... deleted: ./models/barcode copy.py

---
+++ models/client_account.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+""" Barcode class """
+
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class Barcode(BaseModel, Base):
+    """ Representation of Barcode """
+    if models.storage_t == "db":
+        __tablename__ = 'barcode'
+
+        barcode = Column(String(50), primary_key=True)
+        item = Column(Integer, ForeignKey('item.id'), nullable=False)
+
+        item_rel = relationship('Item', backref='barcodes')
+
+    def __init__(self, *args, **kwargs):
+        """ Barcode initialization """
+        super().__init__(*args, **kwargs)
