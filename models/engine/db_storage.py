dd0582bbb8d9ba6f2e51c4fa4378d97c5be00a36
Tundroid <felixkimbu@outlook.com>
2024-10-16 16:25:43 +0100

    Some change...

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -101,6 +101,7 @@ class DBStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
+        print("Why you")
         if cls not in classes.values():
             return None
 
