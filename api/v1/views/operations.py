c26b65a4cdc21592a11e0c6468f10e62b285fde6
Tundroid <felixkimbu@outlook.com>
2024-10-16 17:34:37 +0100

    Some change...

--- api/v1/views/operations.py
+++ api/v1/views/operations.py
@@ -10,7 +10,7 @@ from api.v1.views import app_views
 @app_views.route("/operation", methods=['GET'], strict_slashes=False)
 @app_views.route("/operation/<operation_id>", methods=['GET'], strict_slashes=False)
 def get_operation(operation_id=None):
-    print("hmmm")
+    print("hmmm", operation_id)
     if operation_id:
         operation = storage.get(Operation, operation_id)
         if operation:
