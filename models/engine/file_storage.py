5fc82b0945f8bb70d7f4b3d593fc30335ee11cb4
Tundroid <felixkimbu@outlook.com>
2024-10-16 17:32:51 +0100

    Some change...

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,6 +71,7 @@ class FileStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
+        print("na here")
         if cls not in classes.values():
             return None
 
