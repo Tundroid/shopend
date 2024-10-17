2197955d35480375a92c79720fab8e2fcbf83ac8
Tundroid <felixkimbu@outlook.com>
2024-10-17 11:03:24 +0100

    Some change...

--- models/supply.py
+++ models/supply.py
@@ -11,7 +11,7 @@ class Supply(BaseModel, Base):
     """ Representation of Supply """
     if models.storage_t == "db":
         __tablename__ = 'supply'
-        __table_args__ = {'schema': 'mole_commerce', 'primary_key': None}
+        __table_args__ = {'schema': 'mole_commerce', 'implicit_returning': False}
 
         item = Column(Integer, ForeignKey('mole_commerce.item.id'))
         quantity = Column(Integer, nullable=False)
