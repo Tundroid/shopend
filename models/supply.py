d7dd500d2054b6351c7051121ca5cc7e3a4bc04b
Tundroid <felixkimbu@outlook.com>
2024-10-17 11:10:22 +0100

    Some change...

--- models/supply.py
+++ models/supply.py
@@ -11,7 +11,7 @@ class Supply(BaseModel, Base):
     """ Representation of Supply """
     if models.storage_t == "db":
         __tablename__ = 'supply'
-        __table_args__ = {'schema': 'mole_commerce', 'mysql_engine': 'InnoDB', 'implicit_returning': False}
+        __table_args__ = {'schema': 'mole_commerce'}
 
         id = Column(BigInteger, primary_key=True, autoincrement=True)
         item = Column(Integer, ForeignKey('mole_commerce.item.id'))
