e2b4c99072fcbe71b52f6cf21bee3d22a8915b6f
Tundroid <felixkimbu@outlook.com>
2024-10-22 14:54:33 +0100

    Styling: fix model payment to adhere to pycodestyle

--- models/payment.py
+++ models/payment.py
@@ -3,7 +3,10 @@
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, SmallInteger, String, Date, Text, TIMESTAMP, ForeignKey, Boolean
+from sqlalchemy import (
+    Column, Integer, SmallInteger, String, Date,
+    Text, TIMESTAMP, ForeignKey, Boolean
+)
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 
@@ -15,14 +18,18 @@ class Payment(BaseModel, Base):
         __table_args__ = {'schema': 'mole_commerce'}
 
         id = Column(Integer, primary_key=True, autoincrement=True)
-        invoice = Column(String(50), ForeignKey('mole_commerce.record_detail.batch'))
+        invoice = Column(String(50),
+                         ForeignKey('mole_commerce.record_detail.batch'))
         amount = Column(Integer, nullable=False)
         pay_date = Column(Date, nullable=False)
         is_instant = Column(Boolean, nullable=False, default=True)
-        mode = Column(SmallInteger, ForeignKey('mole_commerce.pay_mode.id'), default=1)
-        app_user = Column(SmallInteger, ForeignKey('mole_commerce.user_account.id'))
+        mode = Column(SmallInteger,
+                      ForeignKey('mole_commerce.pay_mode.id'), default=1)
+        app_user = Column(SmallInteger,
+                          ForeignKey('mole_commerce.user_account.id'))
         pay_desc = Column(Text)
-        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+        datetime = Column(TIMESTAMP, nullable=False,
+                          server_default=func.current_timestamp())
 
         # Establish relationships
         invoice_rel = relationship('RecordDetail', backref='payments')
