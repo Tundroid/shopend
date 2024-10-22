1d39bfd3941ac4776867778264d0a06bdb910529
Tundroid <felixkimbu@outlook.com>
2024-10-22 11:14:06 +0100

    Adding transaction handling

--- api/v1/views/creators.py
+++ api/v1/views/creators.py
@@ -7,6 +7,7 @@ from models import storage
 from api.v1.views import app_views
 from flask_jwt_extended import jwt_required
 from sqlalchemy.exc import IntegrityError
+from models.base_model import Database
 
 @app_views.route("/create", methods=['POST'], strict_slashes=False)
 @app_views.route("/create/<model>", methods=['POST'], strict_slashes=False)
@@ -31,42 +32,15 @@ def create_model(model=None):
             abort(400, description="Valid JSON data required")
 
         data = [data] if type(data) is dict else data
-        for piece in data:
-            db_model = classes[model](**piece)
-            storage.new(db_model)
-            storage.save()
+        db = Database.COMMERCE if classes[model] in classes_commerce.values() else Database.ACCOUNT
+        with storage.transaction(db) as session:
+            for piece in data:
+                db_model = classes[model](**piece)
+                session.new(db_model, db=db)
+                # session.save(db=db)
         
         return jsonify(db_model.to_dict()), 201
     except (KeyError, ValueError) as e:
         return  abort(404, description=f"Model `{model}`")
     except (IntegrityError) as e:
-        print(e.detail)
         return  abort(409, description=f"Resource already exists in Model `{model}`")
-
-
-@app_views.route("/create/<model>/<foreign_key>", methods=['POST'], strict_slashes=False)
-@jwt_required()
-def create_model_foreign_key(model, foreign_key):
-    """Create a new model instance with a foreign key.
-
-    Args:
-        model (str): The model name.
-        foreign_key (str): The foreign key value.
-        request body: JSON data for the new model instance.
-
-    Returns:
-        JSON response with the created model data or an error message.
-    """
-    try:
-        classes = classes_commerce | classes_account
-        data = request.get_json()
-        if not data:
-            abort(400, description="JSON data required")
-        
-        db_model = classes[model](**data, foreign_key=foreign_key)
-        storage.new(db_model)
-        storage.save()
-        
-        return jsonify(db_model.to_dict()), 201
-    except (KeyError, ValueError) as e:
-        return abort(400, description=str(e))
