d4f440a07c5800a4afb1217ee58de7cbd4ff4e57
Tundroid <felixkimbu@outlook.com>
2024-10-22 14:49:37 +0100

    Styling: fix model depot_map to adhere to pycodestyle

--- models/depot_map.py
+++ models/depot_map.py
@@ -3,7 +3,9 @@
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, SmallInteger, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy import (
+    Column, SmallInteger, ForeignKey, PrimaryKeyConstraint
+)
 from sqlalchemy.orm import relationship
 
 
@@ -13,16 +15,25 @@ class DepotMap(BaseModel, Base):
         __tablename__ = 'depot_map'
         __table_args__ = {'schema': 'mole_commerce'}
 
-        source = Column(SmallInteger, ForeignKey('mole_commerce.depot_detail.id'), nullable=False)
-        destination = Column(SmallInteger, ForeignKey('mole_commerce.depot_detail.id'), nullable=False)
-        operation = Column(SmallInteger, ForeignKey('mole_commerce.operation.id'), nullable=False)
+        source = Column(SmallInteger,
+                        ForeignKey('mole_commerce.depot_detail.id'),
+                        nullable=False)
+        destination = Column(SmallInteger,
+                             ForeignKey('mole_commerce.depot_detail.id'),
+                             nullable=False)
+        operation = Column(SmallInteger,
+                           ForeignKey('mole_commerce.operation.id'),
+                           nullable=False)
 
         __table_args__ = (
             PrimaryKeyConstraint('source', 'destination', 'operation'),
         )
 
-        depot_source_rel = relationship('DepotDetail', foreign_keys=[source], backref='depot_map_sources')
-        depot_destination_rel = relationship('DepotDetail', foreign_keys=[destination], backref='depot_map_destinations')
+        depot_source_rel = relationship('DepotDetail', foreign_keys=[source],
+                                        backref='depot_map_sources')
+        depot_destination_rel = relationship('DepotDetail',
+                                             foreign_keys=[destination],
+                                             backref='depot_map_destinations')
         operation_rel = relationship('Operation', backref='depot_maps')
 
     def __init__(self, *args, **kwargs):
