57626d13e2f1e2e78a1776757493265ff1e5b702
Tundroid <felixkimbu@outlook.com>
2024-10-22 14:45:57 +0100

    Styling: fix model client_account to adhere to pycodestyle

--- models/client_account.py
+++ models/client_account.py
@@ -3,7 +3,9 @@
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, SmallInteger, Enum, Boolean, Integer, TIMESTAMP
+from sqlalchemy import (
+    Column, String, SmallInteger, Enum, Boolean, Integer, TIMESTAMP
+)
 from sqlalchemy.sql import func
 
 
@@ -15,10 +17,12 @@ class ClientAccount(BaseModel, Base):
 
         id = Column(SmallInteger, primary_key=True, autoincrement=True)
         acc_name = Column(String(50), nullable=False, unique=True)
-        acc_type = Column(Enum('Consumer', 'Worker', 'Both'), nullable=False, default='Consumer')
+        acc_type = Column(Enum('Consumer', 'Worker', 'Both'),
+                          nullable=False, default='Consumer')
         is_active = Column(Boolean, nullable=False, default=True)
         credit_limit = Column(Integer, nullable=False, default=25000)
-        datetime = Column(TIMESTAMP, nullable=False, server_default=func.current_timestamp())
+        datetime = Column(TIMESTAMP, nullable=False,
+                          server_default=func.current_timestamp())
 
     def __init__(self, *args, **kwargs):
         """ ClientAccount initialization """
